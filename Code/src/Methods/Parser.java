4a3fe3b9ac41bc900c9c0199d1b9e72f19d72a28
MaxRek <max.rekar@gmail.com>
2022-12-08 12:18:12 +0100

    Merge parser

---
+++ Code/src/Methods/Parser.java
@@ -0,0 +1,32 @@
+package Methods;
+
+import Entity.Equipe;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+
+public class Parser{
+    public Equipe equipes[];
+
+    public Parser(String nameFile)throws FileNotFoundException{
+       FileReader file = new  FileReader(nameFile);         
+      Scanner scanner = new Scanner(file);  
+      int nbEquipe = Integer.parseInt(scanner.nextLine());
+      equipes = new Equipe[nbEquipe];
+      String team[] = new String[nbEquipe + 3];
+      int i = 0;
+      int tabGame[] = new int[nbEquipe];
+      while(scanner.hasNextLine())
+      {
+        team = scanner.nextLine().split(" ");
+        tabGame = Arrays.stream(Arrays.copyOfRange(team, 4, 4+nbEquipe)).mapToInt(Integer::parseInt).toArray();
+        equipes[i] = new Equipe(team[1],Integer.parseInt(team[2]),Integer.parseInt(team[3]), tabGame);
+        i = i + 1;
+      }
+    scanner.close();
+  }
+}
--- Code/src/main.java
+++ Code/src/main.java
@@ -1,34 +1,46 @@
-/* Marchau Coralie - Rekar Maxime : Projet Graphes II et Réseaux 2022 */
-import java.lang.reflect.Array;
+// /* Marchau Coralie - Rekar Maxime : Projet Graphes II et Réseaux 2022 */
+// <<<<<<< HEAD
+// import java.lang.reflect.Array;
 
-import Entity.Graph.Graphe;
-import Entity.Graph.GrapheFlots;
-import Methods.TP;
-import Methods.Tools;
+// import Entity.Graph.Graphe;
+// import Entity.Graph.GrapheFlots;
+// import Methods.TP;
+// import Methods.Tools;
+
+// public class main {  
+// 	public  static  void main (String [] arguments) {  		
+// 		int[][] data = 
+// 		{
+// 			{93,8,-1,1,6,1},
+// 			{89,4,1,-1,0,3},
+// 			{88,7,6,0,-1,1},
+// 			{86,5,1,3,1,-1}
+// 		};
+
+// 		for(int i = 0;i<data.length;i++){
+// 			Tools.printIntArray(data[i]);
+// 		}
+// 		System.out.println(data.length);
+// 		int[][] dataM = Tools.copyMatrixWithoutTeam(data, 0);
+// 		for(int i = 0;i<dataM.length;i++){
+// 			Tools.printIntArray(dataM[i]);
+// 		} 
+// 		//GrapheFlots test = TP.init_Graph(data,0);		
+// 		//test.detectErrorFlots();
+// 		//test.init_GraphRes();
+// 		//System.out.println(test);
+// 		//System.out.println(test.getGraphR());
+// 		System.exit(0) ; 
+// =======
+import Entity.*;
+import Entity.ListeSuccesseur.*;
+import Methods.*;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class main {  
-	public  static  void main (String [] arguments) {  		
-		int[][] data = 
-		{
-			{93,8,-1,1,6,1},
-			{89,4,1,-1,0,3},
-			{88,7,6,0,-1,1},
-			{86,5,1,3,1,-1}
-		};
-
-		for(int i = 0;i<data.length;i++){
-			Tools.printIntArray(data[i]);
-		}
-		System.out.println(data.length);
-		int[][] dataM = Tools.copyMatrixWithoutTeam(data, 0);
-		for(int i = 0;i<dataM.length;i++){
-			Tools.printIntArray(dataM[i]);
-		} 
-		//GrapheFlots test = TP.init_Graph(data,0);		
-		//test.detectErrorFlots();
-		//test.init_GraphRes();
-		//System.out.println(test);
-		//System.out.println(test.getGraphR());
-		System.exit(0) ; 
+	public  static  void main (String [] arguments) throws FileNotFoundException{  
+      Parser p = new Parser("../Data/exemple.txt");
+		System.out.println(p.equipes[0].getmatchRestants(0));
 	}
 }
